56ea55fd1159e3aa5c1e580cf5290463a1e1ea1c
bachirbiaich <bachir.biaich@gmail.com>
2018-04-02 17:54:22 +0200

    fix namespace

--- BankCore/Models/User.cs
+++ BankCore/Models/User.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BankAPI.Models
+namespace BankCore.Models
 {
     public class User
     {
